bbc14ac5eb75805e0526a41b4b2d61f81a6f4027
Konstantin S. <havendv@gmail.com>
2026-08-14 19:41:30 +0400

    feat: Updated OpenAPI spec (#39)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Gradium/Generated/Gradium.PronunciationsClient.g.cs
+++ src/libs/Gradium/Generated/Gradium.PronunciationsClient.g.cs
@@ -34,10 +34,17 @@ namespace Gradium
 
         /// <inheritdoc/>
         public global::Gradium.AutoSDKClientOptions Options { get; }
+
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Gradium.SourceGenerationContext.Default);
+
         /// <summary>
         /// 
         /// </summary>
-        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext { get; set; } = global::Gradium.SourceGenerationContext.Default;
+        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext
+        {
+            get => JsonSerializerContextProvider.Value;
+            set => JsonSerializerContextProvider = new(() => value);
+        }
 
 
         /// <summary>
